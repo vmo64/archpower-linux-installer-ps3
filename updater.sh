8663cce9d65897a4fdef62155959bd670df155f2
NGX <45152843+vmo64@users.noreply.github.com>
2026-02-24 23:31:19 +0100

    Update to 0.4

--- updater.sh
+++ updater.sh
@@ -2,5 +2,5 @@
 
 systemctl stop system-manager.service
 mv /usr/local/bin/system-manager/sys-man /usr/local/bin/system-manager/sys-man.bak
-curl -o /usr/local/bin/system-manager/sys-man http://ps3.christianresearchservice.com/archpower/dl/sys-man.sh
+curl -O /usr/local/bin/system-manager/sys-man http://ps3.christianresearchservice.com/archpower/dl/sys-man.sh
 exit;
